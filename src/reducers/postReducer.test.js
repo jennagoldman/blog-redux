0cd329cd03317ff02628d1b96afabcc0107dd01f
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-11 13:09:08 -0700

    passing reducer test for addPost action

--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,2 +1,21 @@
 import { addPost, deletePost, updatePost } from '../actions/postActions.js';
 import reducer from './postReducer.js';
+
+describe('posts reducer', () => {
+  it('handles the addPost action', () => {
+    const state = [];
+    const action = addPost({
+      title: 'Spot is a Good Boy',
+      body: 'In fact, Spot is the Best Boy'
+    })
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual([
+      {
+        title: 'Spot is a Good Boy',
+        body: 'In fact, Spot is the Best Boy'
+      }
+    ])
+  })
+})
