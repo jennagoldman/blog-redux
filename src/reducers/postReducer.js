b2a0e23cf367fe6d1d5fc50a62ca0c5d9fd78430
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-11 13:04:34 -0700

    add switch to reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -2,3 +2,16 @@ import { ADD_POST, DELETE_POST, UPDATE_POST } from '../actions/postActions.js';
 
 const initialState = [];
 
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case ADD_POST:
+      return [...state, action.payload];
+    case DELETE_POST:
+      return state.filter((post, i) => i !== action.payload);
+    case UPDATE_POST:
+      return state.map((post, i) => {
+        if(i === action.payload.index) return action.payload.post;
+        return post;
+      })
+  }
+}
