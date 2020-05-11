9401ecc9d57abd4ed79bc6ce0deb1cfcf203d43b
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-11 13:29:49 -0700

    add test for getPostByIndex selector

--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -18,4 +18,20 @@ describe('posts selectors', () => {
       }
     ])
   })
+
+  it('gets a post by index', () => {
+    const state = [
+      {
+        title: 'Spot is a Good Boy',
+        body: 'In fact, Spot is the Best Boy'
+      }
+    ];
+
+    const post = getPostByIndex(state, 0);
+
+    expect(post).toEqual({
+      title: 'Spot is a Good Boy',
+      body: 'In fact, Spot is the Best Boy'
+    })
+  })
 })
