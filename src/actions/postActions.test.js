b3134062785f9673121bceeb36dbdbcd1cb0350a
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-11 11:21:19 -0700

    updatePost test

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -1,4 +1,4 @@
-import { addPost, ADD_POST, deletePost, DELETE_POST } from './postActions.js';
+import { addPost, ADD_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } from './postActions.js';
 
 describe('post actions', () => {
   it('creates an ADD_POST action', () => {
@@ -22,6 +22,24 @@ describe('post actions', () => {
     expect(action).toEqual({
       type: DELETE_POST,
       payload: 2
+    })
+  })
+
+  it('creates an UPDATE_POST action', () => {
+    const action = updatePost(1, {
+      title: 'Spot is a Good Boy',
+      body: 'No no, he\'s the Bestest Boy'
+    });
+
+    expect(action).toEqual({
+      type: UPDATE_POST,
+      payload: {
+        index: 1,
+        post: {
+          title: 'Spot is a Good Boy',
+          body: 'No no, he\'s the Bestest Boy'
+        }
+      }
     })
   })
 })
